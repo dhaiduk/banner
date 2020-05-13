3b981599c456be53fa7226ec78e3276f8e5aef41
unknown <dementeyyy@gmail.com>
2020-05-13 23:23:48 +0300

    Anime IconApp

--- js/body.js
+++ js/body.js
@@ -1,18 +1,19 @@
 var count;
 var countMeter;
 
+//Get main container banner
+var DOM_mainContainer = document.getElementById("container");
+var WIDTH = DOM_mainContainer.offsetWidth;
+var HEIGHT = DOM_mainContainer.offsetHeight;
+
 //Styles
-var styleBanner = "width: 600px;height: 250px;position: absolute;left: 0;top: 0;";
+var styleBanner = "width: ${WIDTH} px; height: ${HEIGHT} px; position: absolute; left: 0;top: 0; ";
 var styleDino = "height: 150px; position: absolute; left: 2%; top: 30%;";
 var styleCounterMeter = "width: 100px; position: absolute; left: 5px; top: 5px;";
-var styleCounterEnergy = "width: 100px; position: absolute; left: 495px;; top: 5px;";
+var styleCounterEnergy = "width: 100px; position: absolute; left: 545px;; top: 5px;";
 var styleCounterMeterImg = "width: 100px; position: absolute; ";
 var styleCounterEnergyImg = "width: 100px; position: absolute; ";
-var styleAnimation = "animation: animatedBackground 5s linear infinite; @keyframes animatedBackground {	from { background-position: 100% 0; } 	to { background-position: 0 0; } }";
 var styleGameTableMeters = "width: 70px; position: absolute; top: 70%;";
-
-//Get main container banner
-var mainContainer = document.getElementById("container");
 
 //Conten slide 1
 var srcBackground = "images/bg.jpg";
@@ -40,7 +41,6 @@ var srcGameLayer5 = "images/game-layer-5.png";
 var srcGameLayer6 = "images/game-layer-6.png";
 var srcCounterMeter = "images/counter-meters.png";
 var srcCounterEnergy = "images/counter-energy-0.png";
-var srcGameTableMeters = "images/game-table-meters.png";
 
 var DOM_GameLayer1 = document.createElement("img");
 var DOM_GameLayer2 = document.createElement("div");
@@ -48,9 +48,6 @@ var DOM_GameLayer3 = document.createElement("div");
 var DOM_GameLayer4 = document.createElement("div");
 var DOM_GameLayer5 = document.createElement("div");
 var DOM_GameLayer6 = document.createElement("div");
-var DOM_GameTableMeters = document.createElement("div");
-var DOM_GameTableMetersImage = document.createElement("img");
-var DOM_GameTableMetersCount = document.createElement("p");
 var DOM_GameMetersCount = document.createElement("p");
 
 //Counters
@@ -78,26 +75,6 @@ DOM_CounterEnergyImg.src = srcCounterEnergy;
 DOM_CounterEnergyImg.style.cssText = styleCounterEnergyImg;
 DOM_CounterEnergy.appendChild(DOM_CounterEnergyImg);
 
-DOM_GameTableMetersImage.src = srcGameTableMeters;
-DOM_GameTableMetersImage.style.cssText = styleGameTableMeters;
-DOM_GameTableMeters.style.cssText = styleGameTableMeters;
-
-DOM_GameTableMetersCount.style.cssText = styleGameTableMeters;
-DOM_GameTableMetersCount.style.textAlign = "center";
-DOM_GameTableMetersCount.style.top = -25 + "px";
-DOM_GameTableMetersCount.style.fontSize = 30 + "px";
-DOM_GameTableMetersCount.style.fontStyle = "Geneva, Arial, Helvetica, sans-serif";
-DOM_GameTableMetersCount.style.color = "#FFFFFF";
-
-
-DOM_GameMetersCount.style.cssText = styleGameTableMeters;
-DOM_GameMetersCount.style.textAlign = "center";
-DOM_GameMetersCount.style.left = 10 + "px"
-DOM_GameMetersCount.style.top = -10 + "px";
-DOM_GameMetersCount.style.fontSize = 20 + "px";
-DOM_GameMetersCount.style.fontStyle = "Geneva, Arial, Helvetica, sans-serif";
-DOM_GameMetersCount.style.color = "#FFFFFF";
-
 
 //Start Slide One
 function startSlideOne() {
@@ -117,25 +94,29 @@ function startSlideOne() {
 	DOM_IconApp.src = srcIconApp;
 	DOM_IconApp.style.cssText = styleBanner;
 
-
 	DOM_IconsMarkets.src = srcIconsMarkets;
 	DOM_IconsMarkets.style.cssText = styleBanner;
 
-	mainContainer.appendChild(DOM_Background);
-	mainContainer.appendChild(DOM_TextFirstSlide);
-	mainContainer.appendChild(DOM_LogoRastishka);
-	mainContainer.appendChild(DOM_IconApp);
-	mainContainer.appendChild(DOM_IconsMarkets);
+	DOM_mainContainer.appendChild(DOM_Background);
+	DOM_mainContainer.appendChild(DOM_TextFirstSlide);
+	DOM_mainContainer.appendChild(DOM_LogoRastishka);
+	DOM_mainContainer.appendChild(DOM_IconApp);
+	DOM_mainContainer.appendChild(DOM_IconsMarkets);
+	
+
+
+
+
 };
 //End First Slide
 function endSlideOne() {
 	clearTimeout(startTimerslideOne);
 	console.log("endSlideOne");
-	mainContainer.removeChild(DOM_Background);
-	mainContainer.removeChild(DOM_TextFirstSlide);
-	mainContainer.removeChild(DOM_LogoRastishka);
-	mainContainer.removeChild(DOM_IconApp);
-	mainContainer.removeChild(DOM_IconsMarkets);
+	DOM_mainContainer.removeChild(DOM_Background);
+	DOM_mainContainer.removeChild(DOM_TextFirstSlide);
+	DOM_mainContainer.removeChild(DOM_LogoRastishka);
+	DOM_mainContainer.removeChild(DOM_IconApp);
+	DOM_mainContainer.removeChild(DOM_IconsMarkets);
 	startSlideTwo();
 
 };
@@ -143,15 +124,15 @@ function endSlideOne() {
 //Start Slide Two
 function startSlideTwo() {
 	count = 0;
-	mainContainer.appendChild(DOM_GameLayer1);
-	mainContainer.appendChild(DOM_GameLayer2);
-	mainContainer.appendChild(DOM_GameLayer3);
-	mainContainer.appendChild(DOM_GameLayer4);
-	mainContainer.appendChild(DOM_GameLayer5);
-	mainContainer.appendChild(DOM_CounterMeter);
-	mainContainer.appendChild(DOM_CounterEnergy);
-	mainContainer.appendChild(DOM_Dino);
-	mainContainer.appendChild(DOM_GameLayer6);
+	DOM_mainContainer.appendChild(DOM_GameLayer1);
+	DOM_mainContainer.appendChild(DOM_GameLayer2);
+	DOM_mainContainer.appendChild(DOM_GameLayer3);
+	DOM_mainContainer.appendChild(DOM_GameLayer4);
+	DOM_mainContainer.appendChild(DOM_GameLayer5);
+	DOM_mainContainer.appendChild(DOM_CounterMeter);
+	DOM_mainContainer.appendChild(DOM_CounterEnergy);
+	DOM_mainContainer.appendChild(DOM_Dino);
+	DOM_mainContainer.appendChild(DOM_GameLayer6);
 
 	namePlaceMove();
 
@@ -166,9 +147,9 @@ function startSlideTwo() {
 function namePlaceMove() {
 	countMeter = 0;
 	var countText = "" + count;
-	DOM_GameTableMetersCount.innerText = countText;
+	//DOM_GameTableMetersCount.innerText = countText;
 
-	
+
 	count = count + 10;
 
 	let start = Date.now(); // запомнить время начала
@@ -176,36 +157,68 @@ function namePlaceMove() {
 	let timer = setInterval(function () {
 		let timePassed = Date.now() - start;
 		DOM_GameMetersCount.innerText = timePassed;
-		DOM_GameTableMeters.appendChild(DOM_GameTableMetersImage);
-		DOM_GameTableMeters.appendChild(DOM_GameTableMetersCount);
+
 		DOM_CounterMeter.appendChild(DOM_GameMetersCount);
-		mainContainer.appendChild(DOM_GameTableMeters);
-		// сколько времени прошло с начала анимации?
-
-		DOM_GameTableMeters.style.left = mainContainer.offsetWidth - timePassed / 9 + 'px';
-		//DOM_CounterMeter
-		if (DOM_GameTableMeters.offsetLeft <= mainContainer.offsetLeft - DOM_GameTableMeters.offsetWidth) {
-			DOM_GameTableMeters.removeChild(DOM_GameTableMetersImage);
-			DOM_GameTableMeters.removeChild(DOM_GameTableMetersCount);
-			mainContainer.removeChild(DOM_GameTableMeters);
-
-			clearInterval(timer); // закончить анимацию через 2 секунды
-			return;
-		}
 
 	}, 20);
 
-	// в то время как timePassed идёт от 0 до 2000
-	// left изменяет значение от 0px до 400px
-	function draw(timePassed) {
-		DOM_GameTableMeters.style.left = mainContainer.offsetWidth - DOM_GameTableMeters.offsetWidth - timePassed / 5 + 'px';
-		console.log(DOM_GameTableMeters.offsetLeft);
-		if (DOM_GameTableMeters.offsetLeft <= mainContainer.offsetLeft) clearInterval(timer);
-	}
+
 }
 //Step by step run app
 startSlideOne();
 
-mainContainer.addEventListener("mouseup", () => { startTimerslideOne = setTimeout(endSlideOne, 2000); });
-mainContainer.addEventListener("touchend", () => { startTimerslideOne = setTimeout(endSlideOne, 2000); });
+DOM_mainContainer.addEventListener("mouseup", () => { startTimerslideOne = setTimeout(endSlideOne, 2000); });
+DOM_mainContainer.addEventListener("touchend", () => { startTimerslideOne = setTimeout(endSlideOne, 2000); });
 
+
+
+
+
+var RAF = window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	window.oRequestAnimationFrame ||
+	window.msRequestAnimationFrame ||
+	function (callback) { window.setTimeout(callback, 1000 / 60); }
+	;
+
+var ballH = {
+	posX: -200,
+	posY: -500,
+	speedX: 4,
+	speedY: 10,
+	width: 100,
+	height: 100,
+
+	update: function () {
+		var ballElem = DOM_IconApp;
+		ballElem.style.left = this.posX + "px";
+		ballElem.style.top = this.posY + "px";
+	}
+}
+
+var areaH = {
+	width: 400,
+	height: 300
+}
+
+function startAnime() {
+	// синхрон с внутренней анимацией браузера
+	// обычно 60 раз в сек
+	RAF(tick);
+}
+
+function tick() {
+
+	ballH.posX += ballH.speedX;
+	ballH.posY += ballH.speedY;
+	if (ballH.posX >= 0) ballH.posX =0;
+	if (ballH.posY >= 0) ballH.posY =0;
+	ballH.update();
+
+	RAF(tick);
+}
+
+ballH.update();
+
+startAnime();
